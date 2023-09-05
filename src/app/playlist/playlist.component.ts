0560a2da653e3db518bf7fed0e7310da1a714ccf
karanb <karanb@hyperlinkinfosystem.net.in>
2023-09-05 15:05:28 +0530

    Completed

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,12 +1,12 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-playlist',
-  templateUrl: './playlist.component.html',
-  styleUrls: ['./playlist.component.css']
+  selector: "app-playlist",
+  templateUrl: "./playlist.component.html",
+  styleUrls: ["./playlist.component.css"],
 })
 export class PlaylistComponent {
-  songs: string[] = ['radha_ne_shyam_2.0', 'tere_hawale', 'qaafirana'];
+  songs: string[] = ["radha_ne_shyam_2.0", "tere_hawale", "qaafirana"];
   selectedSong: string | undefined;
 
   @Output() selectSongEvent = new EventEmitter<string>();
